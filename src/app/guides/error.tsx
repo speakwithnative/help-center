9c0e05af91ad58db287cc71fbf19cc0c9d852866
Thomas Kaenzig <thomaskaenzig@Thomass-MacBook-Pro.local>
2026-07-05 17:01:31 +0200

    chore: added error file

---
+++ src/app/guides/error.tsx
@@ -0,0 +1,32 @@
+'use client'
+ 
+import { Button } from '@/components/atoms/Button/Button'
+import { useEffect } from 'react'
+ 
+export default function ErrorPage({
+  error,
+  unstable_retry,
+}: {
+  error: Error & { digest?: string }
+  unstable_retry: () => void
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error)
+  }, [error])
+ 
+  return (
+    <div>
+      <h2>Something went wrong!</h2>
+     
+      <Button 
+            onClick={
+                // Attempt to recover by re-fetching and re-rendering the segment
+                () => unstable_retry()
+                }
+            >
+        Try again
+      </Button>
+    </div>
+  )
+}
